1f983ea0535b88d9e3a9eb1c8b6cc1d5462676b2
Valeri Materman <valerima@std.hac.ac.il>
2020-12-15 19:41:40 +0200

    started working on the project

---
+++ include/SidePannel.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+
+class SidePannel
+{
+public:
+	SidePannel(float height_of_window, float widht_of_window);
+
+	void draw(sf::RenderWindow& window);
+
+private:
+	sf::RectangleShape get_inner_rect();
+	sf::RectangleShape get_outer_reect();
+	sf::Vector2f m_location = sf::Vector2f(0, 0);
+	sf::Color m_inside_color = sf::Color(196, 196, 196);
+	sf::Color m_outside_color = sf::Color(169, 169, 169);
+	float m_width;
+	float m_height;
+
+};
